6a57b2985dd848083dfad45311c4964ef573144c
Harshil Dangar <109976669+dangarharshil11@users.noreply.github.com>
2023-09-16 18:49:10 +0530

    Day-2 checkpoint-1

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,3 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <p>Dashboard Page(protected)</p>;
+  return (
+    <div>
+      <p>Dashboard Page(protected)</p>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
