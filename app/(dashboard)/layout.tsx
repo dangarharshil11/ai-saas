46ea37e2cd4b6d2c6a80eb45aa735200e7c9fc02
Harshil Dangar <109976669+dangarharshil11@users.noreply.github.com>
2023-09-22 19:05:17 +0530

    Day-8 checkpoint-3

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,18 @@
 import Navbar from "@/components/navbar";
 import Sidebar from "@/components/sidebar";
+import { getApiLimitCount } from "@/lib/api-limit";
 
-const DaashboardLayout = ({ children }: { children: React.ReactNode }) => {
+const DaashboardLayout = async ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const apiLimitCount = await getApiLimitCount();
+
   return (
     <div className="h-full relative">
       <div className="hidden h-full md:flex md:w-72  md:flex-col md:fixed md:inset-y-0 bg-gray-900 z-[80]">
-        <Sidebar />
+        <Sidebar apiLimitCount={apiLimitCount} />
       </div>
       <main className="md:pl-72">
         <Navbar />
